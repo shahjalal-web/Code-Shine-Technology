354a643755d53507b0b9537ffdefad19d858eafb
mdAbirDeveloper <mdabir.developer@gmail.com>
2025-10-20 09:53:28 +0600

    after remove output export

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: "export",
+  // output: "export",
+  //remove this file for host on vercel
   reactStrictMode: true,
   images: {
     unoptimized: true,
