c45e3ab44e0f0c16581e84f2446854618952ca3e
mdAbirDeveloper <mdabir.developer@gmail.com>
2025-10-20 10:16:21 +0600

    after take chenge on packege.json and config.js

--- next.config.js
+++ next.config.js
@@ -13,7 +13,6 @@
 
 // export default nextConfig;
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -24,4 +23,4 @@ const nextConfig = {
   trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
