0896bbd48b77fd136c4380d428d278493a122f23
mdAbirDeveloper <mdabir.developer@gmail.com>
2025-10-20 10:01:10 +0600

    after remove export

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   // output: "export",
+//   //remove this file for host on vercel
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // output: "export",
-  //remove this file for host on vercel
-  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
